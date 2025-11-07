45e416317efe520c6c22d18277f0a9972fdf791c
Karina Klages <karinaklages@web.de>
2025-11-07 15:30:03 +0100

    refactor: colors and more card details added

---
+++ scripts/templates.js
@@ -0,0 +1,16 @@
+// Template to show one main Pokemon card
+// function getPokemonCard(index) {
+//     return `
+//         <div class="main-card" tabindex="0">
+//             <div class="pokemon-number" id="pokemonNumber"><span>#001</span></div>
+//             <div id="pokemonImgMainCard">
+//                 <img src="./assets/icons/pokemon-logo.svg" alt="">
+//             </div>
+//             <h2 class="pokemon-name-main-card" id="pokemonNameMainCard">Bulbasaur</h2>
+//             <div class="type-area" id="typeArea">
+//                 <button class="pokemon-type-button">Grass</button>
+//                 <button class="pokemon-type-button">Poison</button>
+//             </div>
+//         </div>
+//     `;
+// }
