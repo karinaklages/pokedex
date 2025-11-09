f42d2c8e08f42dd2255d79e2f6bdea433d8114b4
Karina Klages <karinaklages@web.de>
2025-11-09 16:53:33 +0100

    feat: main cards completed

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
+// ---------------------
 // Variables
+// ---------------------
 
 let index = 0;
 let currentPokemonIndex = 0;
 
+const BASE_URL = "https://pokeapi.co/api/v2/";
+
 let loadedPokemon = [];
 
 
+// ---------------------
 // Functions
+// ---------------------
 
 function init() {
-    
+    fetchPokemon();
+    renderPokemon();
 }
 
+// async function fetchPokemon() {
+//     const response = await fetch(BASE_URL + "pokemon?limit=30&offset=0");
+//     const data = await response.json();
+//     loadedPokemon = data.results;
+//     console.log("Pokémon geladen:", loadedPokemon);
+// }
+
+// async function fetchPokemon() {
+//     const response = await fetch(BASE_URL + "pokemon/1/");
+//     const data = await response.json();
+//     loadedPokemon = [data];
+//     console.log(loadedPokemon);
+// }
+
+async function fetchPokemon() {
+    try {
+        const response = await fetch(BASE_URL + "pokemon?limit=30&offset=0");
+        const data = await response.json();
+
+        const detailPromises = data.results.map(p => fetch(p.url).then(res => res.json()));
+
+        loadedPokemon = await Promise.all(detailPromises);
+        console.log(loadedPokemon);
+        renderPokemon();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// showLoadingSpinner()
+
+// await
+
+// disabledloadingSpinner()
+// renderPokemon() 
+
+function renderPokemon() {
+    const pokemonContent = document.getElementById("mainCardArea");
+    pokemonContent.innerHTML = "";
+
+    for (let index = 0; index < loadedPokemon.length; index++) {
+        pokemonContent.innerHTML += getPokemonCard(index);
+    }
+}
+
+
+// ---------------------
+// Dialog / Detail Card
+// ---------------------
+
 function renderDialog(index) {
-    let placeDialog = document.getElementById("pokemonDialog")
+    const placeDialog = document.getElementById("pokemonDialog")
     placeDialog.innerHTML = "";
     placeDialog.innerHTML += dialogTemplate(index);
 }
@@ -40,6 +97,6 @@ function prevPokemon() {
 }
 
 function showTabContentDetailCard(id) {
-  document.querySelectorAll('.content').forEach(el => el.classList.remove('active'));
+  document.querySelectorAll('.content').forEach(element => element.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,19 +1,48 @@
-// Shows one main card
-// function getPokemonCard(index) {
-//     return `
-//         <div class="main-card" tabindex="0" onclick="openDialog()">
-//             <div class="pokemon-number" id="pokemonNumber"><span>#001</span></div>
-//             <div id="pokemonImgMainCard">
-//                 <img src="./assets/icons/pokemon-logo.svg" alt="">
-//             </div>
-//             <h2 class="pokemon-name-main-card" id="pokemonNameMainCard">Bulbasaur</h2>
-//             <div class="type-area" id="typeArea">
-//                 <button class="pokemon-type-button">Grass</button>
-//                 <button class="pokemon-type-button">Poison</button>
-//             </div>
-//         </div>
-//     `;
-// }
+const colorType = {
+    grass: "#c4dfc1",
+    fire: "#eabaaf",
+    water: "#ccd8e8",
+    bug: "#dfd8ca",
+    normal: "#c6c5a6",
+    poison: "#d4c1dd",
+    electric: "#eadda2",
+    ground: "#d7c183",
+    fairy: "#efdad9",
+    fighting: "#f2cb9d",
+    psychic: "#e2c6d0",
+    rock: "#c9c0b4",
+    ghost: "#acaccd",
+    ice: "#def5f5",
+    dragon: "#b9d2d3",
+    dark: "#c7c7c7ff",
+    steel: "#e0e0e0",
+    flying: "#e0e5f5"
+};
+
+
+function getPokemonCard(index) {
+    const backgroundColor = colorType[loadedPokemon[index].types[0].type.name] || "#c6c5a6";
+
+    return `
+        <div class="main-card" tabindex="0" onclick="openDialog()" style="background-color: ${backgroundColor}">
+            <div class="pokemon-number"><span>#${loadedPokemon[index].id}</span></div>
+            <div id="pokemonImgMainCard">
+                <img src="${loadedPokemon[index].sprites.other["official-artwork"].front_default}" alt="Pokémon Illustration">
+            </div>
+            <h2 class="pokemon-name-main-card">${loadedPokemon[index].name}</h2>
+            <div class="type-area" id="typeArea">
+                <button class="pokemon-type-button">${loadedPokemon[index].types[0].type.name}</button>
+                ${
+                    loadedPokemon[index].types[1]
+                        ? `<button class="pokemon-type-button">${loadedPokemon[index].types[1].type.name}</button>`
+                    : ""
+                }
+            </div>
+        </div>
+    `;
+}
+
+// ${loadedPokemon[index].sprites.other.home.front_shiny}
 
 
 // function dialogTemplate(index) {
